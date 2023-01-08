68ccdc19e44044528cd50ee8c41b8c41ac01e228
Jonah Hollis <jonahquinnhollis@gmail.com>
2023-01-08 16:06:46 -0500

    daily coding challenge for 01/18/23

--- js/main.js
+++ js/main.js
@@ -149,3 +149,48 @@ function removeUrlAnchor(url){
   // return the first item in the array, or the url up until the '#' was previously
   return url[0]
 }
+
+// --------------------------------------------------------------
+
+// [7kyu] - DESCRIPTION - Categorize New Member - The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+// Input
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+// Output
+// Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+// Example
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+
+function openOrSenior(data){
+  // [P]arameters - array with array objects of information container member's age and handicap
+  // [R]eturns - returns a string of either 'Open' or 'Senior' depending on array composition ('Senior' defined as age >= 55 & handicap > 7)
+  // [E]xample - input: [[18, 20]] output: 'Open'
+  // [P]seudo Code: 
+  // take in array
+  // create conditional based on array data pairs, age and handicap - handle negative numbers for handicap
+  // return str entry based on conditional result to new array
+  // return new array
+  
+  let results = []
+  
+    data.forEach(([a,b]) => {
+    if(a >= 55 && b > 7){
+      //console.log('Senior')
+      results.push('Senior')
+    }else{
+      //console.log('Open')
+      results.push('Open')
+    }
+
+  
+  })
+  
+  return results
+}
+
