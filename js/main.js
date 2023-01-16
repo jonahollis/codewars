5b1e9b7f7ab0989f06ca2c3a83048d3a5c3e6793
Jonah Hollis <jonahquinnhollis@gmail.com>
2023-01-16 17:10:27 -0500

    adding coding challenge for 01162023

--- js/main.js
+++ js/main.js
@@ -130,11 +130,22 @@ function findShort(s){
 // "GCAT"  =>  "GCAU"
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
-function DNAtoRNA(dna) {
+function dnaToRna(dna) {
   //Parameter: str, combination of 'GCAT'
   //Return: return string
   //Example: 'GCAT' >> 'GCAU'
   //Psuedocode: take string, replace all instances of str 'T' with 'U'
   dna = dna.replaceAll('T','U')
   return dna
+}
+
+// --------------------------------------------------------------
+
+// [7kyu] - DESCRIPTION - Remove anchor from URL - Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+function removeUrlAnchor(url){
+  // split url into two array items at the '#' key
+  url = url.split('#')
+  // return the first item in the array, or the url up until the '#' was previously
+  return url[0]
 }
