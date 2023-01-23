51f12c0ace3b68438a9013d619748b4d74ceac2f
Jonah Hollis <jonahquinnhollis@gmail.com>
2023-01-23 09:26:15 -0500

    adding daily coding challenge for 1/23/23

--- js/main.js
+++ js/main.js
@@ -229,6 +229,38 @@ function sum (arr) {
 
 // --------------------------------------------------------------
 
+// [7kyu] - DESCRIPTION - A square of squares - You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+
+// [P]arameters - integer
+// [R]eturns - returns true if integer is perfect square, false if not
+// [E]xample - input:  4 output: true
+// [P]seudo Code: 
+
+
+// var squared = (n) => Math.sqrt(n) * Math.sqrt(n) === n && Math.sqrt(n) % 1 === 0 ? true : false
+
+var squared = (n) => Math.sqrt(n) * Math.sqrt(n) === n && Math.sqrt(n) % 1 === 0 ? true : false
+
+
+// --------------------------------------------------------------
+
 // [8kyu] - DESCRIPTION - Title - Description text
 
 // [P]arameters - 
